2daf303ae3072e9d77ddbff05a85efad62da828a
kalaimukilan23 <kalaimukilan2001@gmail.com>
2025-02-19 20:43:58 +0530

    commit2

---
+++ calculator1.js
@@ -0,0 +1,40 @@
+const displayText = document.getElementById("valuePara");
+displayText.value = "" ; 
+let num1=0,symbol,num2=0 ; 
+
+function addNum(num){
+    displayText.value += num ;
+    displayText.textContent += num;  
+    
+}
+
+function addSymbolCal(sym){
+    num1 = parseFloat(displayText.value);  
+    displayText.value = "";
+    displayText.textContent = ""; 
+   
+    symbol = sym ;  
+}
+
+function clearAllNum(){
+    displayText.value = "";
+    displayText.textContent = ""; 
+}
+
+function calculate(){
+    num2 = parseFloat(displayText.value);  
+    
+    let result; 
+
+    switch(symbol){
+        case "+": result =  num1+num2  ; break ;
+        case "-": result = num1-num2 ; break ;
+        case "*": result = num1*num2 ; break ;
+        case "/": result = num1/num2 ; break ;
+        case "%": result = num1*(num2/100); break; 
+    }
+
+    displayText.value = result ; 
+    displayText.textContent = result ; 
+
+}
