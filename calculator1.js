6ba016f75c7fddfb0dee840a494dbc8bc232b9b8
kalaimukilan23 <kalaimukilan2001@gmail.com>
2025-02-21 10:38:14 +0530

    commit3

--- calculator1.js
+++ calculator1.js
@@ -5,14 +5,12 @@ let num1=0,symbol,num2=0 ;
 function addNum(num){
     displayText.value += num ;
     displayText.textContent += num;  
-    
 }
 
 function addSymbolCal(sym){
     num1 = parseFloat(displayText.value);  
     displayText.value = "";
     displayText.textContent = ""; 
-   
     symbol = sym ;  
 }
 
@@ -34,7 +32,7 @@ function calculate(){
         case "%": result = num1*(num2/100); break; 
     }
 
-    displayText.value = result ; 
-    displayText.textContent = result ; 
+    displayText.value = `${result}` ; 
+    displayText.textContent = `${result}` ; 
 
 }
